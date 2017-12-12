bbbf49d86e12a3ac6d3e2dcedff0c5c300cbc9ad
Benjamin Pourian <b.pourian@hotmail.co.uk>
2017-12-12 17:07:05 +0000

    vowel challenge refactor

--- vowel_challenge.rb
+++ vowel_challenge.rb
@@ -1,25 +1,21 @@
-#Check if it is a vowel(a, e, i, o, u,) on the n position in a
-#string (the first argument). Don't forget about uppercase.
-#{
-#checkVowel('cat', 1)  ->   true // 'a' is a vowel
-#checkVowel('cat', 0)  ->   false // 'c' is not a vowel
-#checkVowel('cat', 4)  ->   false // this position doesn't eis_vowelist
-#}
-
+# Check if it is a vowel(a, e, i, o, u,) on the n position in a
+# string (the first argument). Don't forget about uppercase.
+# {
+# checkVowel('cat', 1)  ->   true // 'a' is a vowel
+# checkVowel('cat', 0)  ->   false // 'c' is not a vowel
+# checkVowel('cat', 4)  ->   false // this position doesn't eis_vowelist
+# }
 
 def check_vowel(string, position)
-
-  string = string.downcase.split("")
-  vowels = ["a", "e", "i", "o", "u"]
+  string = string.downcase.split('')
+  vowels = %w[a e i o u]
 
   is_vowel = false
 
   vowels.each do |vowel|
-
     is_vowel = true if string[position] == vowel
     break if is_vowel == true
   end
   is_vowel = false if position < 0
   puts is_vowel
-
 end
