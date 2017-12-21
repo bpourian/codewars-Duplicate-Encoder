b26c1455f2f6936edba8fd6e22632ad0d21d8e41
Benjamin Pourian <b.pourian@hotmail.co.uk>
2017-12-21 19:50:54 +0000

    refactored and added comments to solution

--- high_score_table.rb
+++ high_score_table.rb
@@ -23,41 +23,54 @@ highScoreTable.scores == [12, 10, 10]
 highScoreTable.reset()
 highScoreTable.scores == []
 # And so on...
+-----------------
+##alternative solution which I found interesting
+
+class HighScoreTable
+  attr_accessor :scores
+
+  def initialize(limit)
+    @limit = limit
+    @scores = []
+  end
+
+  def update(new_score)
+    @scores << new_score
+    @scores = @scores.sort.reverse.take(@limit)
+  end
+
+  def reset
+    @scores = []
+  end
+end
+-----------------
 =end
 
 
+#my solution
 class HighScoreTable
 
   def initialize(size)
-    #@score_list = []#Array.new(size)
     @size = size
     @score_list = []
   end
 
   def scores
+    #sort array and put in descending order
     @score_list = @score_list.sort.reverse
   end
 
   def update(score)
+    #add to array whilst its length doesn't exceed the size given
     return @score_list << score if @score_list.length < @size
+    #otherwise reverse and sort array in descending order
     @score_list = @score_list.sort.reverse
+    #if last element is smaller than the score, then replace with score
     @score_list[@size - 1] = score if @score_list[@size - 1] < score
   end
-
-
 
   def reset
     @score_list = []
   end
 
 end
-
-highScoreTable = HighScoreTable.new(3)
-highScoreTable.update(10)
-highScoreTable.update(12)
-highScoreTable.update(15)
-highScoreTable.update(11)
-highScoreTable.update(23)
-print highScoreTable.scores
-highScoreTable.reset
-print highScoreTable
