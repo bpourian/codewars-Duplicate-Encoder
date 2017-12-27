85795f1fa1ed34f935a42e447a2d9d8f588aa161
Benjamin Pourian <b.pourian@hotmail.co.uk>
2017-12-27 19:52:42 +0000

    method added not tested on codewars yet

--- ordered_count_of_characters.rb
+++ ordered_count_of_characters.rb
@@ -7,3 +7,10 @@ Example:
 ordered_count("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
 
 =end
+
+def ordered_count(str)
+  str = str.scan(/\w|\s/).inject(Hash.new(0)){|h, c| h[c] += 1; h}
+  str.to_a
+end
+
+ordered_count("abr acadabra")
