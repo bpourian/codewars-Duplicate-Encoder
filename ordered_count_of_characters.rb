7e46428e964c16dc8a4bac81db54599cfcd36e01
Benjamin Pourian <b.pourian@hotmail.co.uk>
2017-12-27 19:13:57 +0000

    first commit

---
+++ ordered_count_of_characters.rb
@@ -0,0 +1,9 @@
+=begin
+Count the number of occurencences of each character and return it as a list of
+tuples in order of appearance.
+
+Example:
+
+ordered_count("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
+
+=end
