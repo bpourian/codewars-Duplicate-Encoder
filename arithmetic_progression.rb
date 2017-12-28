d521deffc8069f090e7cd8ce57e31640ce84c9f0
Benjamin Pourian <b.pourian@hotmail.co.uk>
2017-12-28 10:40:10 +0000

    first commit

---
+++ arithmetic_progression.rb
@@ -0,0 +1,17 @@
+=begin
+In your class, you have started lessons about arithmetic progression.
+Since you are also a programmer, you have decided to write a function that will
+return the first 'n' elements of the sequence with the given common difference
+'r' and first element 'a'. Result should be separated by comma and space.
+
+Example:
+
+arithmetic_sequence_elements(1, 2, 5) == '1, 3, 5, 7, 9'
+
+More info: https://en.wikipedia.org/wiki/Arithmetic_progression
+
+=end
+
+def arithmetic_sequence_elements(a,r,n)
+  #your code here
+end
